67b9bcde6b34d3ab7367b33fc9420fe38eeb7a3f
Gloria <gloghe@yahoo.it>
2022-06-17 12:57:13 +0200

    inizio

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderLandrick/>
+    <!-- jumbotron -->
+  <main></main>
+  <footer></footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderLandrick from './components/HeaderLandrick.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderLandrick,
+
   }
 }
 </script>
@@ -19,10 +22,12 @@ export default {
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #ADB5BD;
+}
+
+*{
+  border: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/HeaderLandrick.vue
@@ -0,0 +1,46 @@
+<template>
+  <header>
+    <img :src="imgLogo" :alt="imgAlt">
+    <nav>
+
+    </nav>
+    <ul >
+        <span>
+            <li v-for="(font, i) in navbarFont" :key="i">{{font}}</li>
+        </span>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderLandrick',
+  data() {
+    return {
+        imgLogo: require('../assets/img/logo-dark.png'),
+        imgAlt: "Logo Landrick",
+        navBar: ["HOME", "LANDING", "PAGES", "DOCS"],
+        navbarFont: [
+            '<font-awesome-icon icon="fa-solid fa-magnifying-glass" />',
+            '<font-awesome-icon icon="fa-brands fa-github" />',
+            '',
+            '<font-awesome-icon icon="fa-solid fa-user" />'
+            ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 70px;
+
+        img{
+            max-width: 140px;
+        }
+    }
+</style>
